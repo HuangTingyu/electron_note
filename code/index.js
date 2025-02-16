ab8320a7c1ea564f0ebce3ec1de6b760bb29a021
HuangTingyu <1007285445@qq.com>
2025-02-16 16:49:21 +0800

    主进程渲染进程通讯

--- code/index.js
+++ code/index.js
@@ -4,15 +4,32 @@
  * @Date: 2025-02-09 14:38:58
  */
 /*
-global require
+global require,process
 */
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const minWindow = function () {
 	let win = new BrowserWindow({
 		width: 800,
-		height: 600
+		height: 600,
+		webPreferences: {
+			zoomFactor: 2, //调大界面
+			nodeIntegration: true,
+			contextIsolation: false
+		}
 	})
 	win.loadFile('./page/index.html')
+
+	ipcMain.on('open-dev-tools', () => {
+		win.webContents.openDevTools()
+	})
+
+	ipcMain.on('close-dev-tools', () => {
+		win.webContents.closeDevTools()
+	})
+
+	ipcMain.handle('get-version', () => {
+		return process.versions
+	})
 }
 app.whenReady().then(() => {
 	minWindow()
