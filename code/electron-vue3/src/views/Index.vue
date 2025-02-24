4457c2a471b5d691fc7bfeb0e120acd070fa7075
HuangTingyu <1007285445@qq.com>
2025-02-24 21:05:18 +0800

    electron_vue3

---
+++ code/electron-vue3/src/views/Index.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: sakurahuang
+ * @Description: 
+ * @Date: 2025-02-24 20:58:33
+-->
+<template>
+  <div>electron</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
